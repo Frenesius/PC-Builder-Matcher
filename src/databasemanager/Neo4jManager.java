1239016151e5d7a11ac6ef122216975ab2003f49
Jiar <Jiarshengy@gmail.com>
2014-11-21 15:21:40 +0000

    Made connection with Neo4j

--- src/databasemanager/Neo4jManager.java
+++ src/databasemanager/Neo4jManager.java
@@ -1,10 +1,38 @@
 package databasemanager;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class Neo4jManager {
+	public static String DB_PATH = "/home/j/Build/graph.db";  //TODO CHANGE LOCATION
 	
+	public GraphDatabaseService openNeo4jDatabase(){
+		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(DB_PATH); 
+		return graphDb;
+	}
 	
-	public void openNeo4jDatabase(){
+	public void closeNeo4jDatabase(GraphDatabaseService graphDb){
+		graphDb.shutdown();
+		System.out.println("Database Closed Successfully...");
+	}
+	
+	public void executeQueryNeo4j(GraphDatabaseService db, String query){
+		Transaction tx =  db.beginTx();		
+		try { 
+			    ExecutionEngine engine = new ExecutionEngine(db);
+			    ExecutionResult result = engine.execute(query);			    
+			    tx.success();
+		       } 
+		 finally {    
+		     tx.close();  
+		 } 
 		
 	}
+	
+	
+	
+	
 	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,36 @@
 package main;
 
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.impl.util.StringLogger;
+
+import databasemanager.Neo4jManager;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Main");
-
-	}
-
+	    Neo4jManager a = new Neo4jManager();
+		GraphDatabaseService db = a.openNeo4jDatabase();
+		
+		String query = "MATCH(n) RETURN n";
+		Transaction tx =  db.beginTx();		
+		 try { 
+			 	//Node n = db.createNode();
+			
+			    ExecutionEngine engine = new ExecutionEngine(db);
+			    ExecutionResult result = engine.execute(query);			    
+			    System.out.println(result);
+			    String stringDump = result.dumpToString();
+		        System.out.println(stringDump);
+			    tx.success();
+		       } 
+		 finally {    
+		     tx.close();  
+		 } 
+	}	
 }
+	
