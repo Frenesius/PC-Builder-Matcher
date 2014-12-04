b5958e33647e0f39aa0ae84215a7a228f16c4aa8
Jiar <Jiarshengy@gmail.com>
2014-12-04 15:52:33 +0000

    Needed to push

---
+++ src/unittest/MatcherMainUnitTest.java
@@ -0,0 +1,17 @@
+package unittest;
+
+import matcher.MatcherMain;
+
+import org.junit.Test;
+
+public class MatcherMainUnitTest  {
+	@Test
+	public void multiplicationOfZeroIntegersShouldReturnZero() {
+
+	   // MyClass is tested
+	   MatcherMain matcherMain = new MatcherMain();
+
+	   // Tests
+	   
+	 } 
+}
